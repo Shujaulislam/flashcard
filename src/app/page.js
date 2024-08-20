4026c9762a552dbd09be4283e4c3dca7412c015a
Abdullah Barudgar <69586404+AbdullahBarudgar@users.noreply.github.com>
2024-08-20 10:35:03 +0530

    contact page and update footer

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
 
       {/* Second Header Section */}
-      <section className="bg-white py-16">
+      <section className=" bg-gradient-to-br from-pink-100 to-purple-100 py-16">
         <div className="container mx-auto px-4">
           <h2 className="text-gray-900 text-3xl font-bold text-center mb-12">Key Features</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -43,7 +43,7 @@ export default function Home() {
       </section>
 
       {/* Third Header Section */}
-      <section className="bg-gray-100 py-16">
+      <section className=" bg-gradient-to-br from-pink-100 to-purple-100 py-16">
         <div className="container mx-auto px-4">
           <h2 className="text-gray-900 text-3xl font-bold text-center mb-12">Our Vision</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
